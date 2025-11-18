197c59d3b6d3ebc4948eeac21abb671848c815f3
Cole J. Calamos <it@calamos.family>
2025-11-18 10:46:17 -0700

    Updated dev shell

--- shell.nix
+++ shell.nix
@@ -2,20 +2,96 @@
   pkgs,
   inputs,
 }: let
-  # Define your zed settings
   zedSettings = {
-    "vim_mode" = false;
-    "relative_line_numbers" = false;
-    "load_direnv" = "shell_hook";
+    deno = {
+      settings = {
+        deno = {
+          enable = true;
+        };
+      };
+    };
+    lsp = {
+      nix = {
+        binary = {
+          path_lookup = true;
+        };
+      };
+      nil = {
+        initialization_options = {
+          formatting = {
+            command = [
+              "alejandra"
+              "--quiet"
+              "--"
+            ];
+          };
+        };
+      };
+      nixd = {
+        initialization_options = {
+          formatting = {
+            command = [
+              "alejandra"
+              "--quiet"
+              "--"
+            ];
+          };
+        };
+      };
+    };
+
+    auto_install_extensions = {
+      "nix" = true;
+      "deno" = true;
+    };
+
+    languages = {
+      JavaScript = {
+        language_servers = [
+          "deno"
+          "!typescript-language-server"
+          "!vtsls"
+          "!eslint"
+        ];
+        formatter = "language_server";
+      };
+      TypeScript = {
+        language_servers = [
+          "deno"
+          "!typescript-language-server"
+          "!vtsls"
+          "!eslint"
+        ];
+        formatter = "language_server";
+      };
+      nix = {
+        formatter = {
+          external = {
+            command = "alejandra";
+            arguments = [
+              "--quiet"
+              "--"
+            ];
+          };
+        };
+      };
+    };
   };
 in
   pkgs.mkShell {
-    packages = [pkgs.deno];
+    packages = [
+      pkgs.deno
+      pkgs.jq
+    ];
+
     buildInputs = [
+      pkgs.alejandra
+      pkgs.nixd
+      pkgs.nil
       (inputs.zed-editor.packages.x86_64-linux.default zedSettings)
     ];
 
     shellHook = ''
-      echo "Using wrapped local zed: ./.direnv/.config/zed"
+      echo "Using Local Zed with Deno & Nix"
     '';
   }
